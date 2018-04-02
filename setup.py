a6470a69e42fa8fb3fdfe5825597362b199ac088
Josh Goates <joshgoates@msn.com>
2018-04-02 09:32:06 -0600

    Adds setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""MachUp_Py: Low-Order Aerodynamics Algorithm"""
+
+from setuptools import setup
+
+setup(name = 'MachUp_Py',
+      version = '1.1',
+      desctiption = 'Low-Order Aerodynamics Algorithm',
+      url = 'https://igthub.com/usuaero/MachUp_Py',
+      author = 'usuaero',
+      author_email = 'doug.hunsaker@usu.edu',
+      license = 'MIT',
+      packages = ['machup'],
+      zip_safe = False)
+
